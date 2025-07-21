80b3e95efa46b48d758af38bc5ba8bebc5b1ee86
Tony Edgin <tedgin@cyverse.org>
2025-07-21 13:45:54 -0700

    Created unittest file for avra.re

---
+++ playbooks/tests/rules/avra.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# © 2025 The Arizona Board of Regents on behalf of The University of Arizona.
+# For license information, see https://cyverse.org/license.
+
+"""Tests of avra.re rule logic."""
+
+import unittest
+
+import test_rules
+from test_rules import IrodsTestCase
+
+
+@test_rules.unimplemented
+class TestAvra(IrodsTestCase):
+    """Tests of avra.re"""
+
+
+if __name__ == "__main__":
+    unittest.main()
