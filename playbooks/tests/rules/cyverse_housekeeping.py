8b0e6117c4045cfeef7bf3ebba271824d3c9ac71
Tony Edgin <tedgin@cyverse.org>
2025-07-21 15:33:00 -0700

    DS-788: created unittest file for cyverse_housekeeping.re

---
+++ playbooks/tests/rules/cyverse_housekeeping.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# © 2025 The Arizona Board of Regents on behalf of The University of Arizona.
+# For license information, see https://cyverse.org/license.
+
+"""Tests of cyverse_housekeeping.re rule logic"""
+
+import unittest
+
+import test_rules
+from test_rules import IrodsTestCase
+
+
+@test_rules.unimplemented
+class TestCyverseHousekeeping(IrodsTestCase):
+    """Tests of cyverse_housekeeping.re"""
+
+
+if __name__ == "__main__":
+    unittest.main()
