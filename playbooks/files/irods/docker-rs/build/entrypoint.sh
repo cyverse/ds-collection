3a8ae3999abb009152df66a539de33f0cdfe6563
Tony Edgin <tedgin@cyverse.org>
2025-10-09 15:31:04 -0700

    DS-764: second pass at log file handling for rs container

--- playbooks/files/irods/docker-rs/build/entrypoint.sh
+++ playbooks/files/irods/docker-rs/build/entrypoint.sh
@@ -4,11 +4,28 @@ set -o errexit -o nounset -o pipefail
 
 export IRODS_USER_PASSWORD
 export IRODS_CATALOG_PROVIDER
+export IRODS_ZONE_NAME
 
-echo "Initializing clerver session"
+main() {
+	log $$ 'Initializing clerver session ...'
 
-su --command="IRODS_HOST='$IRODS_CATALOG_PROVIDER' iinit" irods <<<"$IRODS_USER_PASSWORD" \
-	&> /dev/null
+	su --command="IRODS_HOST='$IRODS_CATALOG_PROVIDER' iinit" irods <<<"$IRODS_USER_PASSWORD" \
+		> /dev/null
 
-echo "Starting server"
-su --command='/usr/sbin/irodsServer -u' irods
+	log $$ 'Starting server ...'
+	su --command='/usr/sbin/irodsServer -u' irods
+}
+
+log() {
+	local pid="$1"
+	local msg="$2"
+
+	local ts
+	ts="$(date --utc '+%FT%T.%N' | sed 's/......$/Z/')"
+
+	printf \
+		'{"log_category":"init","log_level":"info","log_message":"%s","server_host":"%s","server_pid":%s,"server_timestamp":"%s","server_type":"server","server_zone":"%s"}\n' \
+		"$msg" "$HOSTNAME" "$pid" "$ts" "$IRODS_ZONE_NAME"
+}
+
+main "$@"
