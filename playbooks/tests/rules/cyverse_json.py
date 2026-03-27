98ef6cf7bfe1901a9004e8f064904c5efc4b7796
Tony Edgin <tedgin@cyverse.org>
2026-03-27 14:42:41 -0700

    DS-798: fixed copy pasta issue in cyverse_json.re

--- playbooks/tests/rules/cyverse_json.py
+++ playbooks/tests/rules/cyverse_json.py
@@ -27,33 +27,35 @@ class CyverseJsonListTest(IrodsTestCase):
 
     def test_revaccum_empty(self):
         """test _cyverse_json_revAccum with empty list"""
-        revAcc = IrodsVal.string_list(["hi"])
-        self.fn_test("_cyverse_json_revAccum", [revAcc, IrodsVal.string_list([])], revAcc)
+        rev_acc = IrodsVal.string_list(["hi"])
+        self.fn_test("_cyverse_json_revAccum", [rev_acc, IrodsVal.string_list([])], rev_acc)
 
-    @unittest.skip("not implemented")
     def test_revaccum_one_empty_rev(self):
         """
         test _cyverse_json_revAccum with list containing one item and an empty
         reverse list
         """
+        item = IrodsVal.string_list(["hi"])
+        self.fn_test("_cyverse_json_revAccum", [IrodsVal.string_list([]), item], item)
 
-    @unittest.skip("not implemented")
     def test_revaccum_one_nonempty_rev(self):
         """
         test _cyverse_json_revAccum with list containing one item and a
         non-empty reverse list
         """
+        self.fn_test(
+            "_cyverse_json_revAccum",
+            [IrodsVal.string_list(["1"]), IrodsVal.string_list(["2"])],
+            IrodsVal.string_list(["2", "1"]))
 
-    @unittest.skip("not implemented")
-    def test_revaccum_two(self):
-        """test _cyverse_json_revAccum with list containing two items"""
-
-    @unittest.skip("not implemented")
     def test_rev(self):
         """test the _cyverse_json_rev"""
+        self.fn_test(
+            "_cyverse_json_rev",
+            [IrodsVal.string_list(["a", "b"])],
+            IrodsVal.string_list(["b", "a"]))
 
 
-@test_rules.unimplemented
 class CyverseJsonStringTest(IrodsTestCase):
     """Test the string functions in cyverse_json.re"""
 
